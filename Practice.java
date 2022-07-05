97aadc644f6e4ff66bb818459b2932c8f3bc4a55
Josh Miller <107601540+jj0shm@users.noreply.github.com>
2022-07-05 12:02:42 -0500

    new commit being made

---
+++ Practice.java
@@ -0,0 +1,18 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		String Name = "Josh";
+		
+		if( Name == "Josh") { 
+			System.out.println("My Name Is Josh");
+		}
+		else 
+		{ System.out.println("My Name Is Not Josh, I Am Sam");
+		
+		}
+
+	}
+
+}
